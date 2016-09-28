2bc035e96e0bd43f9207d1b912aa492ba56a021e
DavydovDmitriy <Dima051198@yandex.ru>
2016-09-28 22:30:30 +0400

    prog

--- Davydov/lesson 4 (28.09.16)/matrix.cpp
+++ Davydov/lesson 4 (28.09.16)/matrix.cpp
@@ -2,17 +2,23 @@
 #include<cstdlib>
 using namespace std;
 
-void P(int *a,int *b)
+void det(int *a);
 const int l=2;
 int main()
 {
-	int i, j;
-	int A[l][l];
-	int B[l][l];
-	int E[l][l];
+	setlocale(0, " ");
+	int i, j, k;
+	int A[l+1][l+1];
+	int B[l+1][l+1];
+	int E[l+1][l+1];
+	int C[l+1][l+1];
 
-	
-	
+	for (i=1;i<=l;i++)
+	for (j=1;j<=l;j++)
+	{
+	E[i][j] = 0;
+	C[i][j] = 0;
+	}
 
 	for (i=1;i<=l;i++)
 	for (j=1;j<=l;j++)
@@ -38,29 +44,48 @@ int main()
 	}
 	cout<<endl;
 
-	cout<<"The sum of matrices"<<endl;
-	for (i=1;i<=l;i++)
-	{
-	for (j=1;j<=l;j++)
-		cout<<A[i][j]+B[i][j]<<" ";
-		cout<<endl;
-	}
-	cout<<endl;
+		cout<<"The sum of matrices"<<endl;
+		for (i=1;i<=l;i++)
+		{
+		for (j=1;j<=l;j++)
+			{
+			C[i][j] = A[i][j]+B[i][j];
+			cout<<C[i][j]<<" ";
+			}
+			cout<<endl;
+		}
+		det(&C[1][1]);
+
 	cout<<"The difference of matrices"<<endl;
 	for (i=1;i<=l;i++)
 	{
 	for (j=1;j<=l;j++)
-		cout<<A[i][j]-B[i][j]<<" ";
+		{	
+		C[i][j] = A[i][j]-B[i][j];
+		cout<<C[i][j]<<" ";
+		}
 		cout<<endl;
 	}
+	det(&C[1][1]);
+		
+	cout<<"The composition of matrices"<<endl;
+	for (k=1;k<=l;k++)
+	{
+	for (i=1;i<=l;i++)
+	{
+	for (j=1;j<=l;j++)
+		E[k][i]=E[k][i]+A[k][j]*B[j][i];
+		cout<<E[k][i]<<" ";
+	}
 	cout<<endl;
-	
+	}
+	det(&E[1][1]);
 
 	system("pause");
-	return 0;
+	return 0;	
 }
-
-void P(int *a,int *b)
+void det(int *c)
 {
-
+cout<<"The determinat of this matrix is "<<*(c)**(c+4)-*(c+1)**(c+3)<<endl;
+cout<<endl;
 }
