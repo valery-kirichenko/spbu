f368b34f32b91386d474ed6b48c5545270de4382
Valery Kirichenko <valera5505@gmail.com>
2016-09-30 10:30:48 +0300

    Optimize

--- kirichenko/multiply_vectors.cpp
+++ kirichenko/multiply_vectors.cpp
@@ -9,10 +9,10 @@ int main()
     
     for (int i = 0; i < 10; i++)
     	std::cin >> vec1[i];
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++) {
     	std::cin >> vec2[i];
-    for (int i = 0; i < 10; i++)
     	vec1[i] *= vec2[i];
+    }
     
     std::copy(vec1.begin(), vec1.end(), std::ostream_iterator<int>(std::cout, "\n" ) ) ;
  
