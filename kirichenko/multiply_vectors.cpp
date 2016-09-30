fbfeda7a5a44f185d08abed02ae5fc833e2b3e0e
Valery Kirichenko <valera5505@gmail.com>
2016-09-30 10:29:42 +0300

    Vector task

---
+++ kirichenko/multiply_vectors.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <vector>
+#include <iterator>
+#include <algorithm>
+ 
+int main()
+{
+    std::vector<int> vec1(10), vec2(10);
+    
+    for (int i = 0; i < 10; i++)
+    	std::cin >> vec1[i];
+    for (int i = 0; i < 10; i++)
+    	std::cin >> vec2[i];
+    for (int i = 0; i < 10; i++)
+    	vec1[i] *= vec2[i];
+    
+    std::copy(vec1.begin(), vec1.end(), std::ostream_iterator<int>(std::cout, "\n" ) ) ;
+ 
+    return 0;
+}
