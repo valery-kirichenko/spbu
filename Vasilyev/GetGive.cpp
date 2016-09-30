7fcbe9d0c37bbf9deb221c0f8afe922a0c82a0f9
Vasar007 <vasar007@yandex.ru>
2016-09-30 11:03:36 +0300

    Deus Vac

---
+++ Vasilyev/GetGive.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+int inp()
+{
+st:
+	string str;
+	int b = 0, x = 0;
+	bool k = true;
+	str.clear();
+	getline(cin, str);
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (((str[i] < '0') | (str[i] > '9')) & (str[0] != '-')) {
+			k = false;
+			break;
+		}
+		if (str[i] == '-') {
+			b++;
+			if (b > 1) {
+				k = false;
+				break;
+			}
+		}
+	}
+	if (k == false)	{
+		cout << "Error! Wrong data! Try again.\n";
+		goto st;
+	}
+	else {
+		x = stoi(str);
+		return x;
+	}
+	return 0;
+}
+
+int main()
+{
+	int n;
+sv:
+	cout << "Enter size of array: ";
+	n = inp();
+	if (n < 0) {
+		cout << "Error! Wrong size! Try again.\n";
+		goto sv;
+	}
+	vector <int> a(n);
+	for (int i = 0; i < a.size(); i++)
+	{
+		cout << "Enter element " << i + 13 << " : ";
+		a[i] = inp();
+		cout << endl;
+	}
+
+	int min = a[0], max = a[0], sum = 0;
+	for (int i =0; i < a.size(); i++)
+	{
+		if (a[i] < min)
+			min = a[i];
+		if (a[i] > max)
+			max = a[i];
+		sum += a[i];
+	}
+	cout << "Max = " << max << endl;
+	cout << "Min = " << min << endl;
+	cout << "Average = " << sum / a.size() << endl;
+	system("pause");
+	return 0;
+}
