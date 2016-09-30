e4473a6ffb89789e2a1b7687c5ce5b9ae0575dd6
Valery Kirichenko <valera5505@gmail.com>
2016-09-30 10:08:19 +0300

    Two-dimensional dynamic array

--- kirichenko/pointer.cpp
+++ kirichenko/pointer.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 
 int main() {
-	int *b, n;
+	int m, n;
 	std::cout << "Размер массива: ";
-	std::cin >> n;
+	std::cin >> m >> n;
+
+	int **b = new int *[m];
+
+	for (int j = 0; j < m; j++) {
+		b[j] = new int[n];
+		for (int i = 0; i < n; i++) {
+			std::cin >> b[j][i];
+		}
+
+	}
+	for (int j = 0; j < m; j++) {
+		for (int i = 0; i < n; i++) {
+			std::cout << b[j][i] << "\t";
+		}
+		std::cout << std::endl;
+	}
 	
-	b = new int[n];
-
-	for(int i = 0; i < n; i++) {
-		std::cin >> b[i];
-	}
-
-	for(int i = 0; i < n; i++) {
-		std::cout << b[i] << std::endl;
-	}
-
-	delete [] b;
 
 	return 0;
 }
