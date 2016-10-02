cee021bd5cb8400826392f71a1e0cd26e24645f2
DavydovDmitriy <Dima051198@yandex.ru>
2016-10-02 11:18:26 +0400

    lesson 4

--- Davydov/lesson 4 (28.09.16)/matrix.cpp
+++ Davydov/lesson 4 (28.09.16)/matrix.cpp
@@ -4,76 +4,74 @@
 using namespace std;
 
 const int l=2;
-void det(int C[l][l]);
+void det(int E[l][l]);
 int main()
 {
 	setlocale(0, " ");
 	srand ( time(NULL) ); 
 	int i, j, k;
-	int A[l+1][l+1];
-	int B[l+1][l+1];
-	int E[l+1][l+1];
-	int C[l+1][l+1];
+	int A[l][l];
+	int B[l][l];
+	int E[l][l];
 
-	for (i=1;i<=l;i++)
-	for (j=1;j<=l;j++)
-	{
-	E[i][j] = 0;
-	C[i][j] = 0;
-	}
 
-	for (i=1;i<=l;i++)
-	for (j=1;j<=l;j++)
+
+	for (i=0;i<l;i++)
+	for (j=0;j<l;j++)
 	{
 		A[i][j] = rand() % 5 - 2;
 		B[i][j] = rand() % 5 - 2;
 	}
 
 	cout<<"The first matrix"<<endl;
-	for (i=1;i<=l;i++)
+	for (i=0;i<l;i++)
 	{
-	for (j=1;j<=l;j++)
+	for (j=0;j<l;j++)
 		cout<<A[i][j]<<" ";
 		cout<<endl;
 	}
 	cout<<endl;
 	cout<<"The second matrix"<<endl;
-	for (i=1;i<=l;i++)
+	for (i=0;i<l;i++)
 	{
-	for (j=1;j<=l;j++)
+	for (j=0;j<l;j++)
 		cout<<B[i][j]<<" ";
 		cout<<endl;
 	}
 	cout<<endl;
 
 		cout<<"The sum of matrices"<<endl;
-		for (i=1;i<=l;i++)
+		for (i=0;i<l;i++)
 		{
-		for (j=1;j<=l;j++)
+		for (j=0;j<l;j++)
 			{
-			C[i][j] = A[i][j]+B[i][j];
-			cout<<C[i][j]<<" ";
+			E[i][j] = A[i][j]+B[i][j];
+			cout<<E[i][j]<<" ";
 			}
 			cout<<endl;
 		}
-		det(C);
+		det(E);
 
 	cout<<"The difference of matrices"<<endl;
-	for (i=1;i<=l;i++)
+	for (i=0;i<l;i++)
 	{
-	for (j=1;j<=l;j++)
+	for (j=0;j<l;j++)
 		{	
-		C[i][j] = A[i][j]-B[i][j];
-		cout<<C[i][j]<<" ";
+		E[i][j] = A[i][j]-B[i][j];
+		cout<<E[i][j]<<" ";
 		}
 		cout<<endl;
 	}
-	det(C);
+	det(E);
 		
+		for (int i=0;i<l;i++)
+		for (int j=0;j<l;j++)
+		E[i][j] = 0;
+	
 	cout<<"The composition of matrices"<<endl;
-	for (k=1;k<=l;k++)
+	for (k=0;k<l;k++)
 	{
-	for (i=1;i<=l;i++)
+	for (i=0;i<l;i++)
 	{
 	for (j=1;j<=l;j++)
 		E[k][i]=E[k][i]+A[k][j]*B[j][i];
@@ -86,15 +84,11 @@ int main()
 	system("pause");
 	return 0;	
 }
-<<<<<<< HEAD
-void det(int N[l][l])
+
+
+void det(int E[l][l])
 {
-cout<<"The determinat of this matrix is "<<N[1][1]<<endl;
-=======
-void det(int Ñ[l][l])
-{
-cout<<"The determinat of this matrix is "<<Ñ[1][1]<<endl;
->>>>>>> 2be1a7a266c197c27276441a93588cbafe02f4ae
+cout<<"The determinat of this matrix is "<<E[0][0]*E[1][1]-E[0][1]*E[1][0]<<endl;
 cout<<endl;
 }
 
