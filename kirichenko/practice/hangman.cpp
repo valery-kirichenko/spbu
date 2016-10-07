a1cc1a7d3d01c05113b1430a44295be11074a1a2
Valery Kirichenko <valera5505@gmail.com>
2016-10-07 18:38:00 +0300

    ...

--- kirichenko/practice/hangman.cpp
+++ kirichenko/practice/hangman.cpp
@@ -84,6 +84,10 @@ std::string to_lower(std::string s) {
 	return out;
 }
 
+// Some magic needed to work with cyrillic letters, cause they weight 2 positions
+// when _ symbol weight 1 position. We use additional array indices to calculate
+// position for needed symbol, given like for normal string (e.g. for string "пр_вет" 
+// normal position for letter в is 3, but in C++ we have 5)
 int sym_to_pos(int sym, std::string word, int indices[]) {
 	int sum = 0;
 	for (int i = 0; i < sym; i++)
@@ -93,20 +97,20 @@ int sym_to_pos(int sym, std::string word, int indices[]) {
 
 void game(std::string word) {
 	int attempt = 0;
-	int letter_count = word.length() / 2;
+	int letter_count = word.length() / 2; // Division by 2 was explained above
 	std::string mod_word = word.substr(0, 2);
 	for (int i = 1; i < letter_count - 1; i++)
 		mod_word += "_";
-	mod_word += word.substr(letter_count*2 - 2, 2);
+	mod_word += word.substr(letter_count*2 - 2, 2); // Take from 2 to penultimate letter
 	std::string guess, letter;
 	bool exist;
 
-	int mod_ind[letter_count];
+	int mod_ind[letter_count]; // Array for index calculation
 	for (int i = 0; i < letter_count; i++) {
-		mod_ind[i] = 1;
+		mod_ind[i] = 1; // 1 where the symbol _
 	}
-	mod_ind[0] = 2;
-	mod_ind[letter_count - 1] = 2;
+	mod_ind[0] = 2; // And 2 where first...
+	mod_ind[letter_count - 1] = 2; // ..and last letters
 	int ind[letter_count];
 	for (int i = 0; i < letter_count; i++)
 		ind[i] = 2;
@@ -129,17 +133,17 @@ void game(std::string word) {
 		exist = false;
 
 		for (int i = 1; i < letter_count - 1; i++) {
-			letter = word.substr(sym_to_pos(i, word, ind), 2);
-			if (letter == guess) {
+			letter = word.substr(sym_to_pos(i, word, ind), 2); // Take each letter
+			if (letter == guess) { // Compare it with guessed one
 				exist = true;
-				mod_word.replace(sym_to_pos(i, mod_word, mod_ind), mod_ind[i], guess);
-				mod_ind[i] = 2;
+				mod_word.replace(sym_to_pos(i, mod_word, mod_ind), mod_ind[i], guess); // Replace placeholder
+				mod_ind[i] = 2; // And modify appropriate index
 				guessed_letters++;
 				letters.push_back(guess);
 			}
 		}
 
-		if (guessed_letters == letter_count)
+		if (guessed_letters == letter_count) // If all letters guessed (player won)
 			attempt += 10;
 	
 		if (!exist)
@@ -163,12 +167,12 @@ void play_computer() {
 	std::string word, mod_word;
 	int word_count = 0;
 	while (std::getline(dict, word))
-        ++word_count;
+        ++word_count; // Calculate lines in the words file (each line is a word)
 	dict.clear();
-	dict.seekg(0, std::ios::beg);
+	dict.seekg(0, std::ios::beg); // Return to the beggining of the file
 	std::random_device generator;
 	std::uniform_int_distribution<int> distribution(0, word_count);
-	int rand = distribution(generator);
+	int rand = distribution(generator); // Select random word
 	for (int i = 0; i < rand; i++)
 		std::getline(dict, word);
 	dict.close();
@@ -179,7 +183,7 @@ void play_friend() {
 	std::string word;
 	std::cout << "Загадайте слово:\n";
 	std::cin >> word;
-	std::cout << "\033[2F\033[K";
+	std::cout << "\033[2F\033[K"; // Remove word that was entered by second player
 	word = to_lower(word);
 	if (word == "false") {
 		std::cout << "Слово должно состоять из букв русского алфавита\n";
