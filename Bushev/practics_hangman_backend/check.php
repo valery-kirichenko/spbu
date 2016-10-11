c09f09ebbfc660d40d9d116a27b7dcd203a26a6e
= <slavok1732@hotmail.com>
2016-10-11 20:47:54 +0400

    fixed bugs

---
+++ Bushev/practics_hangman_backend/check.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: text/html; charset=cp1251');
+if($_POST['hash'] != "HCXt7xC47HGvVwo_68XX25Tgs789XBZ4fhsP0") exit("#Ошибка, доступ запрещен");
+
+include_once("mysql/sql_connect.php");
+
+$gid = (int)$_POST['gid'];
+$data = mysqli_query($db, "SELECT player FROM hangman_games WHERE gid='$gid'") or exit();
+$fdata = mysqli_fetch_assoc($data);
+
+if(isset($fdata['player']) && !empty($fdata['player'])) exit($fdata['player']);
+?>
