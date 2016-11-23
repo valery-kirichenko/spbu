976cc2fb1646aedb4e2f13b60ffbf8f64dd4879c
Valery Kirichenko <valera5505@gmail.com>
2016-11-23 12:14:38 +0300

    some changes

--- kirichenko/socket/client.cpp
+++ kirichenko/socket/client.cpp
@@ -2,7 +2,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-//#include <netinet/in.h>
 #include <iostream>
 
 #define SIZE 2000
@@ -20,32 +19,32 @@ int main() {
 	std::cout << "Socket created" << std::endl;
 
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8888);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8888);
 
-    //Connect to server
-    if (connect(socket_desc, (struct sockaddr*) &server, sizeof(server)) < 0) {
-        std::cout << "Connection failed" << std::endl;
-        return 1;
-    }
-    std::cout << "Connected" << std::endl;
+	//Connect to server
+	if (connect(socket_desc, (struct sockaddr*) &server, sizeof(server)) < 0) {
+		std::cout << "Connection failed" << std::endl;
+		return 1;
+	}
+	std::cout << "Connected" << std::endl;
 
-    while (true) {
-    	std::cout << "Message: ";
-    	std::cin >> message;
+	while (true) {
+		std::cout << "Message: ";
+		std::cin >> message;
 
-    	if (send(socket_desc, message, strlen(message), 0) < 0) {
-            std::cout << "Send failed" << std::endl;
-            return 1;
-        }
+		if (send(socket_desc, message, strlen(message), 0) < 0) {
+			std::cout << "Send failed" << std::endl;
+			return 1;
+		}
 
-        if (recv(socket_desc, server_reply, SIZE, 0) < 0) {
-            std::cout << "recv failed" << std::endl;
-            break;
-        }
+		if (recv(socket_desc, server_reply, SIZE, 0) < 0) {
+			std::cout << "recv failed" << std::endl;
+			break;
+		}
 
-        std::cout << "Reply: " << server_reply << std::endl;
-    }
+		std::cout << "Reply: " << server_reply << std::endl;
+	}
 
 	return 0;
 }
--- kirichenko/socket/server.cpp
+++ kirichenko/socket/server.cpp
@@ -11,43 +11,43 @@ int main() {
 	struct sockaddr_in server, client;
 
 	//Create socket
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_desc == -1) {
-        std::cout << "Could not create socket" << std::endl;
-        return 1;
-    }
-    std::cout << "Socket created" << std::endl;
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_desc == -1) {
+		std::cout << "Could not create socket" << std::endl;
+		return 1;
+	}
+	std::cout << "Socket created" << std::endl;
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8888);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
 
-    if (bind(socket_desc, (struct sockaddr*) &server, sizeof(server)) < 0) {
-    	std::cout << "Bind failed" << std::endl;
-    	return 1;
-    }
-    std::cout << "Bind done" << std::endl;
+	if (bind(socket_desc, (struct sockaddr*) &server, sizeof(server)) < 0) {
+		std::cout << "Bind failed" << std::endl;
+		return 1;
+	}
+	std::cout << "Bind done" << std::endl;
 
-    listen(socket_desc, 3);
+	listen(socket_desc, 3);
 
-    std::cout << "Waiting for connection" << std::endl;
+	std::cout << "Waiting for connection" << std::endl;
 
-    c = sizeof(struct sockaddr_in);
-    client_sock = accept(socket_desc, (struct sockaddr*) &client, (socklen_t*) &c);
-    std::cout << "Connection established" << std::endl;
+	c = sizeof(struct sockaddr_in);
+	client_sock = accept(socket_desc, (struct sockaddr*) &client, (socklen_t*) &c);
+	std::cout << "Connection established" << std::endl;
 
-    int read_size;
-    char *message, client_message[SIZE];
+	int read_size;
+	char *message, client_message[SIZE];
 
-    while ((read_size = recv(client_sock, client_message, SIZE, 0)) > 0) {
-    	write(client_sock, client_message, strlen(client_message));
-    }
+	while ((read_size = recv(client_sock, client_message, SIZE, 0)) > 0) {
+		write(client_sock, client_message, strlen(client_message));
+	}
 
-    if(read_size == 0) {
-        std::cout << "Client disconnected" << std::endl;
-    } else if (read_size == -1) {
-        std::cout << "recv failed" << std::endl;
-    }
+	if(read_size == 0) {
+		std::cout << "Client disconnected" << std::endl;
+	} else if (read_size == -1) {
+		std::cout << "recv failed" << std::endl;
+	}
 
-    return 0;
+	return 0;
 }
