67b52f0f46ae6391cc950d0643d49b802d82d05c
stspbu <slavok1717@gmail.com>
2016-12-21 05:33:45 +0300

    Update travis.py

--- travis.py
+++ travis.py
@@ -7,14 +7,14 @@ reqid = os.environ['TRAVIS_PULL_REQUEST']
 js = requests.get('https://api.github.com/repos/valera5505/SPbU/pulls/' + reqid + '/files').json()
 
 for i in range(0, len(js)-1):
-    if fl[i]['status'] == 'removed':
+    if js[i]['status'] == 'removed':
         continue
-    if not fl[i]['filename'].endswith('.cpp'):
+    if not js[i]['filename'].endswith('.cpp'):
         continue
-    print(fl[i]['filename'] + ' is compiling')
-    process = subprocess.Popen(['g++', '-o', fl[i]['filename'] + '.' + i,fl[i]['filename']], stdout=subprocess.PIPE)
+    print(js[i]['filename'] + ' is compiling')
+    process = subprocess.Popen(['g++', '-o', js[i]['filename'] + '.' + i, js[i]['filename']], stdout=subprocess.PIPE)
     process.wait()
 
-    if not os.path.exists(fl[i]['filename'] + '.' + i):
+    if not os.path.exists(js[i]['filename'] + '.' + i):
         exit(1)
     
