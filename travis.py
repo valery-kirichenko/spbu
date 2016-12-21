268ee5ee8f8d38f8c48f0921d785c30ace5f302e
stspbu <slavok1717@gmail.com>
2016-12-21 05:28:32 +0300

    Update travis.py

--- travis.py
+++ travis.py
@@ -15,6 +15,6 @@ for i in range(0, len(js)-1):
     process = subprocess.Popen(['g++', '-o', fl[i]['filename'] + '.' + i,fl[i]['filename']], stdout=subprocess.PIPE)
     process.wait()
 
-    if os.path.exists(fl[i]['filename'] + '.' + i) is False:
-	return 1
+    if not os.path.exists(fl[i]['filename'] + '.' + i):
+        exit(1)
     
