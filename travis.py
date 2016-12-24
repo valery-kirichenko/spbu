e4b87e579d457d07d72601a77bfdf289f8c229dd
stspbu <slavok1732@hotmail.com>
2016-12-25 00:31:44 +0300

    check vers

--- travis.py
+++ travis.py
@@ -13,6 +13,9 @@ for i in range(len(js)):
         continue
     print(js[i]['filename'] + ' is compiling')
 	
+	
+    process = subprocess.Popen(['gcc --version'], stdout=subprocess.PIPE)
+	
     process = subprocess.Popen(['i686-w64-mingw32-g++', '-o', js[i]['filename'] + '.' + str(i), js[i]['filename']], stdout=subprocess.PIPE)
     process.wait()
 
