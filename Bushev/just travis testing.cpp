d3aeb3235f70ff12f68224652aa6a150c9066e20
stspbu <slavok1732@hotmail.com>
2017-01-17 02:49:14 +0300

    threads test

--- Bushev/just travis testing.cpp
+++ Bushev/just travis testing.cpp
@@ -1,5 +1,21 @@
 #include <thread>
+#include <iostream>
 
-int main(){
+using namespace std;
+
+const int FLOOD_COUNT = 2500;
+
+void flood() {
+	for (int i = 0; i < FLOOD_COUNT; i++)
+		cout << "flood: " << i << endl;
+}
+
+int main() {
+	thread mthread(flood);
+	mthread.detach();
+
+	for (int i = 0; i < FLOOD_COUNT; i++)
+		cout << "main: " << i << endl;
+
 	return 0;
 }
