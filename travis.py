50d7c9e915a61417992e5cb3cffd803c643720d9
stspbu <slavok1732@hotmail.com>
2017-01-17 03:48:35 +0300

    545

--- travis.py
+++ travis.py
@@ -6,24 +6,19 @@ import requests
 reqid = os.environ['TRAVIS_PULL_REQUEST']
 js = requests.get('https://api.github.com/repos/valera5505/SPbU/pulls/' + reqid + '/files').json()
 
-f = open('Bushev/stdafx.h', 'a+')
-f.write('#include <stdio.h>')
-f.write('#include <tchar.h>')
-f.close();
-
-if os.path.exists('Bushev/stdafx.h'):
-    print('created')
-
-print('abvgd')
-process = subprocess.Popen(['ls'], stdout=subprocess.PIPE)
-
 for i in range(len(js)):
     if js[i]['status'] == 'removed':
         continue
     if not js[i]['filename'].endswith('.cpp'):
         continue
     print(js[i]['filename'] + ' is compiling')
-	
+
+	if not os.path.exists(os.path.dirname(file) + '/stdafx.h'):
+        f = open(os.path.dirname(file) + '/stdafx.h', 'a+')
+        f.write('#include <stdio.h>\n')
+        f.write('#include <tchar.h>')
+        f.close();
+    
     process = subprocess.Popen(['g++-6', '-o', js[i]['filename'] + '.' + str(i), js[i]['filename'], '-std=c++14', '-pthread'], stdout=subprocess.PIPE)
     process.wait()
 
