827722ba90e04672ce06856a1660069cfd38ebb5
stspbu <slavok1732@hotmail.com>
2017-01-17 04:05:03 +0300

    ttr

--- travis.py
+++ travis.py
@@ -13,8 +13,9 @@ for i in range(len(js)):
         continue
     print(js[i]['filename'] + ' is compiling')
 
-    if not os.path.exists(s.path.dirname(js[i]['filename']) + '/stdafx.h'):
-        f = open(s.path.dirname(js[i]['filename']) + '/stdafx.h', 'a+')
+    if not os.path.exists(os.path.dirname(js[i]['filename']) + '/stdafx.h'):
+        f = open(os.path.dirname(js[i]['filename']) + '/stdafx.h', 'a+')
+        f.write('#include <stdio.h>')
         f.close();
     
     process = subprocess.Popen(['g++-6', '-o', js[i]['filename'] + '.' + str(i), js[i]['filename'], '-std=c++14', '-pthread'], stdout=subprocess.PIPE)
