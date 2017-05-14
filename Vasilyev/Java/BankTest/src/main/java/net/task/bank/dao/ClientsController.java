a1c304d27040740e083c9bf73f8ed470e19770f0
Vasar007 <vasar007@yandex.ru>
2017-05-14 18:20:21 +0300

    Update Embedded BD

--- Vasilyev/Java/BankTest/src/main/java/net/task/bank/dao/ClientsController.java
+++ Vasilyev/Java/BankTest/src/main/java/net/task/bank/dao/ClientsController.java
@@ -14,25 +14,28 @@ public class ClientsController implements DBClientsController {
     @Autowired
     private JdbcTemplate template;
 
-    final String CLIENTS_TABLE_NAME = "clients";
-    final String INSERT_QUERY = "INSERT INTO " + CLIENTS_TABLE_NAME + " (ID, firstName, middleName, lastName, phone, passport, oldPassport, birthday)" +
-                   "VALUES (?,?,?,?,?,?,?,?);";
+    private final String CLIENTS_TABLE_NAME = "clients";
+    private final String INSERT_QUERY = "INSERT INTO " + CLIENTS_TABLE_NAME +
+            " (ID, firstName, middleName, lastName, phone, passport, oldPassport, birthday)" +
+            "VALUES (?,?,?,?,?,?,?,?);";
 
     @Override
     public void saveNewClient(Client toCreate) {
-        template.execute(INSERT_QUERY, (PreparedStatementCallback<Object>) preparedStatement -> {
-            java.sql.Date sqlDate = new java.sql.Date(toCreate.getBirthday().getTime());
+        if (!isExisted(toCreate)) {
+            template.execute(INSERT_QUERY, (PreparedStatementCallback<Object>) preparedStatement -> {
+                java.sql.Date sqlDate = java.sql.Date.valueOf(toCreate.getBirthday());
 
-            preparedStatement.setInt(1, toCreate.getID());
-            preparedStatement.setString(2, toCreate.getFirstName());
-            preparedStatement.setString(3, toCreate.getMiddleName());
-            preparedStatement.setString(4, toCreate.getLastName());
-            preparedStatement.setString(5, toCreate.getPhone());
-            preparedStatement.setInt(6, toCreate.getPassport());
-            preparedStatement.setInt(7, toCreate.getOldPassport());
-            preparedStatement.setDate(8, sqlDate);
-            return preparedStatement.execute();
-        });
+                preparedStatement.setInt(1, toCreate.getID());
+                preparedStatement.setString(2, toCreate.getFirstName());
+                preparedStatement.setString(3, toCreate.getMiddleName());
+                preparedStatement.setString(4, toCreate.getLastName());
+                preparedStatement.setString(5, toCreate.getPhone());
+                preparedStatement.setString(6, toCreate.getPassport());
+                preparedStatement.setString(7, toCreate.getOldPassport());
+                preparedStatement.setDate(8, sqlDate);
+                return preparedStatement.execute();
+            });
+        }
     }
 
     @Override
@@ -42,17 +45,25 @@ public class ClientsController implements DBClientsController {
 
     @Override
     public void deleteClient(int clientID) {
-        template.execute("DELETE FROM " + CLIENTS_TABLE_NAME + " WHERE id = " + clientID);
+        if (isExisted(getByID(clientID)))
+            template.execute("DELETE FROM " + CLIENTS_TABLE_NAME + " WHERE ID = " + clientID);
     }
 
     @Override
     public Client getByID(int clientID) {
-        return template.query("SELECT * FROM " + CLIENTS_TABLE_NAME + " WHERE id = " + clientID,
+        return template.query("SELECT * FROM " + CLIENTS_TABLE_NAME + " WHERE ID = " + clientID,
                 new ClientRowMapper()).stream().findFirst().orElse(null);
     }
 
     @Override
     public void updateClient(Client toUpdate) {
-        //template.update("UPDATE " + CLIENTS_TABLE_NAME + " SET  = " + toUpdate.getCredits() + " WHERE id = " + toUpdate.getID());
+        //template.update("UPDATE " + CLIENTS_TABLE_NAME + " SET  = " + toUpdate.getCredits() +
+        // " WHERE ID = " + toUpdate.getID());
+        // work in progress
+    }
+
+    @Override
+    public boolean isExisted(Client client) {
+        return (client != null && getByID(client.getID()) != null);
     }
 }
--- Vasilyev/Java/BankTest/src/main/java/net/task/bank/rsdata/DataReader.java
+++ Vasilyev/Java/BankTest/src/main/java/net/task/bank/rsdata/DataReader.java
@@ -4,10 +4,11 @@ import net.task.bank.models.Client;
 import net.task.bank.models.Credit;
 
 import java.io.File;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -15,7 +16,8 @@ public class DataReader implements Reader {
     public List<Client> readClients(File fileClients) {
         List<Client> clients = new ArrayList<>();
 
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+        formatter = formatter.withLocale(Locale.getDefault());
 
         //934->>>>Елена---Водохлёбова\\\\Федоровна~~~~+79646546254==559253___1945-10-01==613027 (\n)
         //934  Елена  Водохлёбова  Федоровна  +79646546254  559253  1945-10-01  (not compulsory)613027
@@ -30,10 +32,10 @@ public class DataReader implements Reader {
                 client.setLastName(userArr[2]);
                 client.setMiddleName(userArr[3]);
                 client.setPhone(userArr[4]);
-                client.setPassport(Integer.parseInt(userArr[5]));
-                client.setBirthday(format.parse(userArr[6]));
+                client.setPassport(userArr[5]);
+                client.setBirthday(LocalDate.parse(userArr[6]));
                 if (userArr.length > 7) {
-                    client.setOldPassport(Integer.parseInt(userArr[7]));
+                    client.setOldPassport(userArr[7]);
                 }
                 clients.add(client);
             }
@@ -47,7 +49,8 @@ public class DataReader implements Reader {
     public List<Credit> readCredits(File fileCredits) {
         List<Credit> credits = new ArrayList<>();
 
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+        formatter = formatter.withLocale(Locale.getDefault());
 
         //Credit{727|515584.72710622626,5.0%,269089.711338759^541363.9634615376___2012-12-31& (next credit)
         //727  515584.72710622626  5.0%  269089.711338759  541363.9634615376  2012-12-31
@@ -62,7 +65,7 @@ public class DataReader implements Reader {
                 credit.setPercent(Double.parseDouble(userArr[2].substring(0, userArr[2].length() - 2)));
                 credit.setPaidSum(Double.parseDouble(userArr[3]));
                 credit.setNeedPaid(Double.parseDouble(userArr[4]));
-                credit.setClosingDate(format.parse(userArr[5]));
+                credit.setClosingDate(LocalDate.parse(userArr[5]));
                 credits.add(credit);
             }
         } catch (Exception ex) {
